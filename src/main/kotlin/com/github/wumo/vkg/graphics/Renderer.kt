95be49e284e877d9b3b26079f32f8c9a191b75bf
WuMo <wumo@outlook.com>
2020-07-08 21:04:19 +0800

    default

--- src/main/kotlin/com/github/wumo/vkg/graphics/Renderer.kt
+++ src/main/kotlin/com/github/wumo/vkg/graphics/Renderer.kt
@@ -5,67 +5,68 @@ import com.github.wumo.vkg.graphics.util.bytes
 import com.github.wumo.vkg.graphics.util.notNull
 
 class WindowConfig(
-    val title: String = "Window",
-    val width: Int = 1280, val height: Int = 720,
-    val fullscreen: Boolean = false,
-    val vsync: Boolean = false,
-    val numFrames: Int = 2,
-    val safeSync: Boolean = false
+  val title: String = "Window",
+  val width: Int = 1280, val height: Int = 720,
+  val fullscreen: Boolean = false,
+  val vsync: Boolean = false,
+  val numFrames: Int = 2,
+  val safeSync: Boolean = false
 )
 
 class FeatureConfig(val atmosphere: Boolean = false,
                     val validationLayer: Boolean = false)
 
 class DeferredSceneConfig(
-    val maxNumVertex: Int = 10_000_000,
-    val maxNumIndex: Int = 10_000_000,
-    val maxNumTransform: Int = 100_000,
-    val maxNumMaterial: Int = 10_000,
-    val maxNumMeshes: Int = 1_000_000,
-    val maxNumLineMeshes: Int = 1_000,
-    val maxNumTransparentMeshes: Int = 1_000,
-    val maxNumTransparentLineMeshes: Int = 1_000,
-    val maxNumTexture: Int = 1_000,
-    val maxNumLights: Int = 1,
-    val sampleCount: Int = 1
+  val maxNumVertex: Int = 10_000_000,
+  val maxNumIndex: Int = 10_000_000,
+  val maxNumTransform: Int = 100_000,
+  val maxNumMaterial: Int = 10_000,
+  val maxNumMeshes: Int = 1_000_000,
+  val maxNumLineMeshes: Int = 1_000,
+  val maxNumTransparentMeshes: Int = 1_000,
+  val maxNumTransparentLineMeshes: Int = 1_000,
+  val maxNumTexture: Int = 1_000,
+  val maxNumLights: Int = 1,
+  val sampleCount: Int = 1
 )
 
 class RayTracingSceneConfig(
-    val maxNumVertex: Int = 10_000_000,
-    val maxNumIndex: Int = 10_000_000,
-    val maxNumTransform: Int = 100_000,
-    val maxNumMaterial: Int = 10_000,
-    val maxNumMeshes: Int = 1_000_000,
-    val maxNumLineMeshes: Int = 1_000,
-    val maxNumTransparentMeshes: Int = 1_000,
-    val maxNumTransparentLineMeshes: Int = 1_000,
-    val maxNumTexture: Int = 1_000,
-    val maxNumLights: Int = 1,
-    val sampleCount: Int = 1,
-    val maxRecursion: Int = 3
+  val maxNumVertex: Int = 10_000_000,
+  val maxNumIndex: Int = 10_000_000,
+  val maxNumTransform: Int = 100_000,
+  val maxNumMaterial: Int = 10_000,
+  val maxNumMeshes: Int = 1_000_000,
+  val maxNumLineMeshes: Int = 1_000,
+  val maxNumTransparentMeshes: Int = 1_000,
+  val maxNumTransparentLineMeshes: Int = 1_000,
+  val maxNumTexture: Int = 1_000,
+  val maxNumLights: Int = 1,
+  val sampleCount: Int = 1,
+  val maxRecursion: Int = 3
 )
 
 class PathTracingSceneConfig(
-    val maxNumVertex: Int = 10_000_000,
-    val maxNumIndex: Int = 10_000_000,
-    val maxNumTransform: Int = 100_000,
-    val maxNumMaterial: Int = 10_000,
-    val maxNumMeshes: Int = 1_000_000,
-    val maxNumLineMeshes: Int = 1_000,
-    val maxNumTransparentMeshes: Int = 1_000,
-    val maxNumTransparentLineMeshes: Int = 1_000,
-    val maxNumTexture: Int = 1_000,
-    val maxNumLights: Int = 1,
-    val sampleCount: Int = 1,
-    val maxRecursion: Int = 3
+  val maxNumVertex: Int = 10_000_000,
+  val maxNumIndex: Int = 10_000_000,
+  val maxNumTransform: Int = 100_000,
+  val maxNumMaterial: Int = 10_000,
+  val maxNumMeshes: Int = 1_000_000,
+  val maxNumLineMeshes: Int = 1_000,
+  val maxNumTransparentMeshes: Int = 1_000,
+  val maxNumTransparentLineMeshes: Int = 1_000,
+  val maxNumTexture: Int = 1_000,
+  val maxNumLights: Int = 1,
+  val sampleCount: Int = 1,
+  val maxRecursion: Int = 3
 )
 
 class Renderer(internal val native: CRenderer,
                val featureConfig: FeatureConfig,
-               internal val closeFunc: () -> Unit
-) : AutoCloseable {
+               internal val closeFunc: ()->Unit
+): AutoCloseable {
   companion object {
-    fun newBasicRenderer(windowConfig: WindowConfig, featureConfig: FeatureConfig = FeatureConfig()): Renderer {
+    fun newBasicRenderer(windowConfig: WindowConfig = WindowConfig(),
+                         featureConfig: FeatureConfig = FeatureConfig()): Renderer {
       val titleBytes = windowConfig.title.bytes()
       val windowConfig_ = CWindowConfig().also {
         it.title(titleBytes)
@@ -89,8 +90,10 @@ class Renderer(internal val native: CRenderer,
         featureConfig_.deallocate()
       }
     }
-
-    fun newDeferredRenderer(windowConfig: WindowConfig, sceneConfig: DeferredSceneConfig, featureConfig: FeatureConfig = FeatureConfig()): Renderer {
+    
+    fun newDeferredRenderer(windowConfig: WindowConfig = WindowConfig(),
+                            sceneConfig: DeferredSceneConfig = DeferredSceneConfig(),
+                            featureConfig: FeatureConfig = FeatureConfig()): Renderer {
       val titleBytes = windowConfig.title.bytes()
       val windowConfig_ = CWindowConfig().also {
         it.title(titleBytes)
@@ -128,8 +131,10 @@ class Renderer(internal val native: CRenderer,
         sceneConfig_.deallocate()
       }
     }
-
-    fun newRayTracingRenderer(windowConfig: WindowConfig, sceneConfig: RayTracingSceneConfig, featureConfig: FeatureConfig = FeatureConfig()): Renderer {
+    
+    fun newRayTracingRenderer(windowConfig: WindowConfig = WindowConfig(),
+                              sceneConfig: RayTracingSceneConfig = RayTracingSceneConfig(),
+                              featureConfig: FeatureConfig = FeatureConfig()): Renderer {
       val titleBytes = windowConfig.title.bytes()
       val windowConfig_ = CWindowConfig().also {
         it.title(titleBytes)
@@ -168,8 +173,10 @@ class Renderer(internal val native: CRenderer,
         sceneConfig_.deallocate()
       }
     }
-
-    fun newPathTracingRenderer(windowConfig: WindowConfig, sceneConfig: PathTracingSceneConfig, featureConfig: FeatureConfig = FeatureConfig()): Renderer {
+    
+    fun newPathTracingRenderer(windowConfig: WindowConfig = WindowConfig(),
+                               sceneConfig: PathTracingSceneConfig = PathTracingSceneConfig(),
+                               featureConfig: FeatureConfig = FeatureConfig()): Renderer {
       val titleBytes = windowConfig.title.bytes()
       val windowConfig_ = CWindowConfig().also {
         it.title(titleBytes)
@@ -209,7 +216,7 @@ class Renderer(internal val native: CRenderer,
       }
     }
   }
-
+  
   val scene = SceneManager(RendererGetSceneManager(native.notNull()))
   val atmosphere = Atmosphere(RendererGetAtmosphere(native.notNull()))
   var wireframe: Boolean = RendererGetWireFrame(native.notNull())
@@ -218,8 +225,8 @@ class Renderer(internal val native: CRenderer,
       field = value
     }
   val window: Window = Window(RendererGetWindow(native.notNull()))
-  fun loop(update: (Double) -> Unit) {
-    val updater = object : CallFrameUpdater() {
+  fun loop(update: (Double)->Unit) {
+    val updater = object: CallFrameUpdater() {
       override fun update(elapsedDuration: Double) {
         update(elapsedDuration)
       }
@@ -227,7 +234,7 @@ class Renderer(internal val native: CRenderer,
     RendererLoopUpdater(native.notNull(), CCallFrameUpdater(updater))
     updater.deallocate()
   }
-
+  
   override fun close() {
     closeFunc()
     native.deallocate()
