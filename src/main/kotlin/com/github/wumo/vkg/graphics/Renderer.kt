cb97a74e2495cfdbc4c6e02729996d0b265b1a87
wumo <wumo@outlook.com>
2020-07-13 20:26:30 +0800

    safeSync default true

--- src/main/kotlin/com/github/wumo/vkg/graphics/Renderer.kt
+++ src/main/kotlin/com/github/wumo/vkg/graphics/Renderer.kt
@@ -5,65 +5,65 @@ import com.github.wumo.vkg.graphics.util.bytes
 import com.github.wumo.vkg.graphics.util.notNull
 
 class WindowConfig(
-  val title: String = "Window",
-  val width: Int = 1280, val height: Int = 720,
-  val fullscreen: Boolean = false,
-  val vsync: Boolean = false,
-  val numFrames: Int = 2,
-  val safeSync: Boolean = false
+    val title: String = "Window",
+    val width: Int = 1960, val height: Int = 1080,
+    val fullscreen: Boolean = false,
+    val vsync: Boolean = false,
+    val numFrames: Int = 2,
+    val safeSync: Boolean = true
 )
 
 class FeatureConfig(val atmosphere: Boolean = false,
                     val validationLayer: Boolean = false)
 
 class DeferredSceneConfig(
-  val maxNumVertex: Int = 10_000_000,
-  val maxNumIndex: Int = 10_000_000,
-  val maxNumTransform: Int = 100_000,
-  val maxNumMaterial: Int = 10_000,
-  val maxNumMeshes: Int = 1_000_000,
-  val maxNumLineMeshes: Int = 1_000,
-  val maxNumTransparentMeshes: Int = 1_000,
-  val maxNumTransparentLineMeshes: Int = 1_000,
-  val maxNumTexture: Int = 1_000,
-  val maxNumLights: Int = 1,
-  val sampleCount: Int = 1
+    val maxNumVertex: Int = 10_000_000,
+    val maxNumIndex: Int = 10_000_000,
+    val maxNumTransform: Int = 100_000,
+    val maxNumMaterial: Int = 10_000,
+    val maxNumMeshes: Int = 1_000_000,
+    val maxNumLineMeshes: Int = 1_000,
+    val maxNumTransparentMeshes: Int = 1_000,
+    val maxNumTransparentLineMeshes: Int = 1_000,
+    val maxNumTexture: Int = 1_000,
+    val maxNumLights: Int = 1,
+    val sampleCount: Int = 1
 )
 
 class RayTracingSceneConfig(
-  val maxNumVertex: Int = 10_000_000,
-  val maxNumIndex: Int = 10_000_000,
-  val maxNumTransform: Int = 100_000,
-  val maxNumMaterial: Int = 10_000,
-  val maxNumMeshes: Int = 1_000_000,
-  val maxNumLineMeshes: Int = 1_000,
-  val maxNumTransparentMeshes: Int = 1_000,
-  val maxNumTransparentLineMeshes: Int = 1_000,
-  val maxNumTexture: Int = 1_000,
-  val maxNumLights: Int = 1,
-  val sampleCount: Int = 1,
-  val maxRecursion: Int = 3
+    val maxNumVertex: Int = 10_000_000,
+    val maxNumIndex: Int = 10_000_000,
+    val maxNumTransform: Int = 100_000,
+    val maxNumMaterial: Int = 10_000,
+    val maxNumMeshes: Int = 1_000_000,
+    val maxNumLineMeshes: Int = 1_000,
+    val maxNumTransparentMeshes: Int = 1_000,
+    val maxNumTransparentLineMeshes: Int = 1_000,
+    val maxNumTexture: Int = 1_000,
+    val maxNumLights: Int = 1,
+    val sampleCount: Int = 1,
+    val maxRecursion: Int = 3
 )
 
 class PathTracingSceneConfig(
-  val maxNumVertex: Int = 10_000_000,
-  val maxNumIndex: Int = 10_000_000,
-  val maxNumTransform: Int = 100_000,
-  val maxNumMaterial: Int = 10_000,
-  val maxNumMeshes: Int = 1_000_000,
-  val maxNumLineMeshes: Int = 1_000,
-  val maxNumTransparentMeshes: Int = 1_000,
-  val maxNumTransparentLineMeshes: Int = 1_000,
-  val maxNumTexture: Int = 1_000,
-  val maxNumLights: Int = 1,
-  val sampleCount: Int = 1,
-  val maxRecursion: Int = 3
+    val maxNumVertex: Int = 10_000_000,
+    val maxNumIndex: Int = 10_000_000,
+    val maxNumTransform: Int = 100_000,
+    val maxNumMaterial: Int = 10_000,
+    val maxNumMeshes: Int = 1_000_000,
+    val maxNumLineMeshes: Int = 1_000,
+    val maxNumTransparentMeshes: Int = 1_000,
+    val maxNumTransparentLineMeshes: Int = 1_000,
+    val maxNumTexture: Int = 1_000,
+    val maxNumLights: Int = 1,
+    val sampleCount: Int = 1,
+    val maxRecursion: Int = 3
 )
 
 class Renderer(internal val native: CRenderer,
                val featureConfig: FeatureConfig,
-               internal val closeFunc: ()->Unit
-): AutoCloseable {
+               internal val closeFunc: () -> Unit
+) : AutoCloseable {
   companion object {
     fun newBasicRenderer(windowConfig: WindowConfig = WindowConfig(),
                          featureConfig: FeatureConfig = FeatureConfig()): Renderer {
@@ -90,7 +90,7 @@ class Renderer(internal val native: CRenderer,
         featureConfig_.deallocate()
       }
     }
-    
+
     fun newDeferredRenderer(windowConfig: WindowConfig = WindowConfig(),
                             sceneConfig: DeferredSceneConfig = DeferredSceneConfig(),
                             featureConfig: FeatureConfig = FeatureConfig()): Renderer {
@@ -131,7 +131,7 @@ class Renderer(internal val native: CRenderer,
         sceneConfig_.deallocate()
       }
     }
-    
+
     fun newRayTracingRenderer(windowConfig: WindowConfig = WindowConfig(),
                               sceneConfig: RayTracingSceneConfig = RayTracingSceneConfig(),
                               featureConfig: FeatureConfig = FeatureConfig()): Renderer {
@@ -173,7 +173,7 @@ class Renderer(internal val native: CRenderer,
         sceneConfig_.deallocate()
       }
     }
-    
+
     fun newPathTracingRenderer(windowConfig: WindowConfig = WindowConfig(),
                                sceneConfig: PathTracingSceneConfig = PathTracingSceneConfig(),
                                featureConfig: FeatureConfig = FeatureConfig()): Renderer {
@@ -216,7 +216,7 @@ class Renderer(internal val native: CRenderer,
       }
     }
   }
-  
+
   val scene = SceneManager(RendererGetSceneManager(native.notNull()))
   val atmosphere = Atmosphere(RendererGetAtmosphere(native.notNull()))
   var wireframe: Boolean = RendererGetWireFrame(native.notNull())
@@ -225,8 +225,8 @@ class Renderer(internal val native: CRenderer,
       field = value
     }
   val window: Window = Window(RendererGetWindow(native.notNull()))
-  fun loop(update: (Double)->Unit) {
-    val updater = object: CallFrameUpdater() {
+  fun loop(update: (Double) -> Unit) {
+    val updater = object : CallFrameUpdater() {
       override fun update(elapsedDuration: Double) {
         update(elapsedDuration)
       }
@@ -234,7 +234,7 @@ class Renderer(internal val native: CRenderer,
     RendererLoopUpdater(native.notNull(), CCallFrameUpdater(updater))
     updater.deallocate()
   }
-  
+
   override fun close() {
     closeFunc()
     native.deallocate()
