0d0c5bfc80907b5feb5127c293f16ba2bdfbdc51
wumo <wumo@outlook.com>
2020-07-14 11:01:16 +0800

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
   `maven-publish`
   kotlin("jvm") version "1.3.72"
   id("com.google.osdetector") version "1.6.2"
-  id("com.github.wumo.javacpp") version "1.0.11"
+  id("com.github.wumo.javacpp") version "1.0.13"
 }
 
 group = "com.github.wumo"
